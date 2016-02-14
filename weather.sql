14899d35faa27f495e2e87269dedeccde9baca3f
Balázs <Meszaros.Balazs.2@stud.u-szeged.hu>
2016-02-14 14:38:31 +0100

    Update weather.sql

--- weather.sql
+++ weather.sql
@@ -7,15 +7,14 @@ else:  # Python 3
     from urllib.request import urlopen
 import json
 
+#Use your own appid after registration
 def get_temp(lat, lon):
-    data = urlopen(
-        "http://api.openweathermap.org/data/2.5/find?lat=%s&lon=%s&cnt=1"
-        % (lat, lon))
+    data = urlopen("http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=YOURIDHERE&cnt=1" % (lat, lon))
     js_data = json.loads(data.read().decode('utf-8'))
     try:
-        return js_data['list'][0]['main']['temp'] - 273.15
+        return js_data['main']['temp'] - 273.15
     except (KeyError, IndexError):
         return None
 
-return get_temp(lat, lon)
+    return get_temp(lat, lon)
 $$ LANGUAGE plpythonu;
